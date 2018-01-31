c4ce1ba1fdb8127bdb3e5021a43222361b81c529
Claire Pate <clairepate@Claires-MacBook-Air.local>
2018-01-30 19:45:00 -0500

    WIP

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var _ = require("underscore");
 // Sets up the Express App
 // =============================================================
 var app = express();
@@ -27,8 +27,37 @@ app.use(express.static('public'));
 
 
 var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+var hbs = exphbs.create({
+  // Specify helpers which are only registered on this instance. 
+  defaultLayout: "main", 
+  helpers: {
+    everyNth: function (context, every, options) {
+      console.log(context);
+      console.log(every);
+      console.log(options);
+      var fn = options.fn, inverse = options.inverse;
+      var ret = "";
+      if (context && context.length > 0) {
+        for (var i = 0, j = context.length; i < j; i++) {
+          var modZero = i % every === 0;
+          ret = ret + fn(_.extend({}, context[i], {
+            isModZero: modZero,
+            isModZeroNotFirst: modZero && i > 0,
+            isLast: i === context.length - 1
+          }));
+        }
+      } else {
+        ret = inverse(this);
+      }
+      return ret;
+    }
+  }
+});
+
+
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
 
 // Routes
 // =============================================================
